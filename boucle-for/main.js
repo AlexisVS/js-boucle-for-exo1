b0112c06366555984e8c82f813fe2175c6c81efb
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2021-04-28 20:39:43 +0200

    Add files via upload

---
+++ boucle-for/main.js
@@ -0,0 +1,16 @@
+let eleves = ["Alexis", "Andy", "Charles", "Dorian", "Farhad", "Ilyas", "Junior", "Kev", "Loïc", "Mihai", "Oussama", "Tania", "William", "Alain"];
+
+// -  # Exo 1 Boucles FOR
+//     ## - Creer une varaible de type array et inserez toutes les personnes de la classe
+//     ## - A l'aide d'une boucle for, dites `bonjour + prenom` à chaque personne du tableau
+
+
+// - # Exo 2 Boucles FOR (let)
+
+for (let i = 0; i < eleves.length; i++) {
+    console.log("Bonjour " + eleves[i])
+}
+let nombre = 5
+for (let j = 0; j <= nombre; j++) {
+    console.log( 0 + j)
+}
